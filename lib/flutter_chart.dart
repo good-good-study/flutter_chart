b3dc2add65d1a2d9ca32dd558c2f1ac9a3607c92
sunxingtao <xt.sun@izhaohu.com>
2019-05-19 22:07:03 +0800

    添加了饼图

--- lib/flutter_chart.dart
+++ lib/flutter_chart.dart
@@ -1,8 +1,10 @@
 library flutter_chart;
 
 export 'package:flutter_chart/chart/chart_bean.dart';
+export 'package:flutter_chart/chart/chart_pie_bean.dart';
 export 'package:flutter_chart/chart/view/chart_bar.dart';
 export 'package:flutter_chart/chart/view/chart_line.dart';
+export 'package:flutter_chart/chart/view/chart_pie.dart';
 
 /// A Calculator.
 class Calculator {
